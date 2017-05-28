56989c304c969c6cf3e62e6de182802233a36698
hisurga <hisurga@gmail.com>
2017-05-28 23:11:24 +0900

    make baseballWEB.py

---
+++ baseballWEB.py
@@ -0,0 +1,47 @@
+import lxml.html
+import requests
+from time import sleep
+from datetime import datetime
+
+DENA = "ＤｅＮＡ";
+
+# 日付情報から某サイトのURLを取得
+today = datetime.now()
+#topUrl = "https://baseball.yahoo.co.jp/npb/schedule/?date={today:%Y%m%d}"
+topUrl = "https://baseball.yahoo.co.jp/npb/schedule/?date=20170530"
+
+# html及びroot取得
+topHtml = requests.get(topUrl).text
+topRoot = lxml.html.fromstring(topHtml)
+
+# 週間予定からtoday pl7で今日の試合予定パスを取り、そこからDENAの文字列を検索
+tdTeam = topRoot.xpath('//td[@class="today pl7"]/*[text()="%s"]' %DENA)
+
+if tdTeam == []:
+    print("今日は試合がないようです。")
+    exit()
+
+# trの位置からscoreUrlを探したい
+tr = tdTeam[0].getparent().getparent()
+
+# scoreUrlの場所はtoday ctの2番目
+forScoreUrl = tr.xpath('td[@class="today ct"][2]//a/@href')
+
+if forScoreUrl == []:
+    print("今日は試合がまだのようです。")
+    exit()
+
+scoreUrl = "http://baseball.yahoo.co.jp/live/" + forScoreUrl[0] + "score";
+scoreHtml = requests.get(scoreUrl).text
+scoreRoot = lxml.html.fromstring(scoreHtml)
+
+# liveNaviの場所に{攻撃中のチーム or 試合終了}が表示されている。
+liveNavi = scoreRoot.cssselect('#livenavi p')[0].text_content()
+
+# 試合中であった場合、batterspanから背番号取得
+if DENA in liveNavi:
+    print(f"{scoreRoot.cssselect('#batter span')[0].text_content()}が打席に立ってます。応援しましょう。")
+elif "試合終了" in liveNavi:
+    print("試合は終了しました。")
+else:
+    print("相手の攻撃中です。")
--- ras-baseballWEB.py
+++ ras-baseballWEB.py
@@ -11,19 +11,18 @@ def cheerPlayer(uniformNo):
 
 if __name__ == "__main__":
     today = datetime.now()
-#    topUrl = "https://baseball.yahoo.co.jp/npb/schedule/?date={today:%Y%m%d}"
-    topUrl = "https://baseball.yahoo.co.jp/npb/schedule/?date=20170525"
+    topUrl = "https://baseball.yahoo.co.jp/npb/schedule/?date={today:%Y%m%d}"
+    #topUrl = "https://baseball.yahoo.co.jp/npb/schedule/?date=20170525"
+
     topHtml = requests.get(topUrl).text
     topRoot = lxml.html.fromstring(topHtml)
     
-    listTeam = topRoot.xpath('//td[@class="today pl7"]/a')
-    linkScore = topRoot.xpath('//td[@class="today ct"]/a/@href')
+    #listTeam = topRoot.xpath('//td[@class="today pl7"]/a')
+    #linkScore = topRoot.xpath('//td[@class="today ct"]/a/@href')
 
     listTeam = topRoot.xpath('//td[@class="today pl7"]/*[text()="%s"]' %DENA)
-    
     team = listTeam[0].getparent().getparent()
 
-    print (team.xpath('td[@class="today ct"][2]//a/@href'))
 
 #    print(lxml.html.tostring(listTeam[0]))
 #    print(listTeam[0].text_content())
@@ -32,26 +31,27 @@ if __name__ == "__main__":
         print("今日は試合が{ないorまだ}のようです。")
         exit()
 
-    #試合開始時間がずれるとリンクもずれてしまう。
-    #xpathやcssselectについて
-    #おそらく解決
+    gameUrl = "http://baseball.yahoo.co.jp/live/" + team.xpath('td[@class="today ct"][2]//a/@href')[0] + "score";
+        
+    '''
     for num in range(len(listTeam)):
         if listTeam.pop(0).text_content() == DENA:
             gameUrl = "http://baseball.yahoo.co.jp/live/" + linkScore[num // 2] + "score";
             break
-    
+
     if gameUrl == "":
         print("今日は試合が{まだのor終了した}ようです。")
         exit()
+
+    '''
 
     gameHtml = requests.get(gameUrl).text
     gameRoot = lxml.html.fromstring(gameHtml)
 
     if DENA in gameRoot.cssselect('#livenavi p')[0].text_content():
-        print(f"{root.cssselect('#batter span')[0].text_content()}が打席に立ってます。応援しましょう。")
-        
-#        ●●●    
+        print(f"{gameRoot.cssselect('#batter span')[0].text_content()}が打席に立ってます。応援しましょう。")
 
+#        ●●●
 '''
     while True:
         gameHtml = requests.get(gameUrl).text
