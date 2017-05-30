b665d7833e165923b679af6322153b5888bfe07f
hisurga <hisurga@gmail.com>
2017-05-30 23:22:46 +0900

    split function

--- ras-baseballWEB.py
+++ ras-baseballWEB.py
@@ -3,10 +3,11 @@ import requests
 from time import sleep
 from datetime import datetime
 
-def fetchGameStatus():
-    DENA = "ＤｅＮＡ"
-    OUT3 = "●●●"
-    
+#DENA = "ＤｅＮＡ"
+DENA = "楽天"
+OUT3 = "●●●"
+
+def fetchScoreUrl():   
     today = datetime.now()
     topUrl = "https://baseball.yahoo.co.jp/npb/schedule/?date={today:%Y%m%d}"
 
@@ -18,7 +19,7 @@ def fetchGameStatus():
     tdTeam = topRoot.xpath('//td[@class="today pl7"]/*[text()="%s"]' %DENA)
 
     if tdTeam == []:
-        return(0, "NOGAME")
+        return("NOGAME")
 
     # trの位置からscoreUrlを探したい
     tr = tdTeam[0].getparent().getparent()
@@ -27,46 +28,50 @@ def fetchGameStatus():
     forScoreUrl = tr.xpath('td[@class="today ct"][2]//a/@href')
 
     if forScoreUrl == []:
-        return(0, "NOTBEGAN")
+        return("NOTBEGAN")
+    else:
+        return(forScoreUrl[0])
 
-    scoreUrl = "http://baseball.yahoo.co.jp/live/" + forScoreUrl[0] + "score";
+def fetchGameStatus(forScoreUrl):
+    scoreUrl = "http://baseball.yahoo.co.jp/live/" + forScoreUrl + "score";
     scoreHtml = requests.get(scoreUrl).text
     scoreRoot = lxml.html.fromstring(scoreHtml)
     
     # liveNaviの場所に{攻撃中のチーム or 試合終了}が表示されている。
     liveNavi = scoreRoot.cssselect('#livenavi p')[0].text_content()
-        
+
+    outCount = ""
     if(scoreRoot.xpath('//p[@class="o"]/b') != []):
         outCount = scoreRoot.xpath('//p[@class="o"]/b')[0].text_content()
         
     # 試合中であった場合batterspanからスライスで背番号を取得し、int型にして返す
     if (DENA in liveNavi) and not(OUT3 in outCount):
-        return(1, int(scoreRoot.cssselect('#batter span')[0].text_content()[1:]))
+        return(1, scoreRoot.cssselect('#batter span')[0].text_content()[1:])
     elif "試合終了" in liveNavi:
         return(0, "OVER")
     else:
         return(1, "OPPONENT")
     
 if __name__ == "__main__":
-    status, detail = fetchGameStatus()
+    forScoreUrl = fetchScoreUrl()
+    if not('/' in forScoreUrl):
+        exit
+    else:
+        while True:
+            status, detail = fetchGameStatus(forScoreUrl)
 
-    while True:
-        if status == 0:
-            if "NOGAME" in detail:
-                print("今日は試合がないようです。")
-                break
-            elif "NOTBEGAN" in detail:
-                print("今日は試合がまだのようです。")
+            if status == 0:
+                if "NOGAME" in detail:
+                    print("今日は試合がないようです。")
+                elif "NOTBEGAN" in detail:
+                    print("今日は試合がまだのようです。")
+                else:
+                    print("試合は終了しました。")
                 break
             else:
-                print("試合は終了しました。")
-                break
-        else:
-            if "OPPONENT" in detail:
-                print("相手の攻撃中です。")
-                sleep(15)
-            else:
-                print(detail + "が打席に立ってます。応援しましょう。")
-                sleep(15)
-    
-            
+                if "OPPONENT" in detail:
+                    print("相手の攻撃中です。")
+                    sleep(15)
+                else:
+                    print(detail + "が打席に立ってます。応援しましょう。")
+                    sleep(15)
