5125545af992a9df2112aa81fc4279432a1c3b92
hisurga <hisurga@gmail.com>
2017-06-11 19:25:57 +0900

    update about fans

--- ras_cheerDeNA.py
+++ ras_cheerDeNA.py
@@ -1,29 +1,38 @@
 from ras_fetchGame import fetchScoreUrl
 from ras_fetchGame import fetchGameStatus
 from ras_playMusic import playMusic
+from ras_operateFans import initPins
+from ras_operateFans import operateFans
 
 forScoreUrl = fetchScoreUrl()
+
 if not('/' in forScoreUrl):
     if "NOGAME" in forScoreUrl:
         print("今日は試合がないようです。")
     else:
         print("今日は試合がまだのようです。")        
     exit
+
 else:
     playMusic("D")
+    initPins()    
     while True:
         status = fetchGameStatus(forScoreUrl)
         
         if "OPPONENT" in status:
+            operateFans(0)
             print("相手の攻撃中です。")
             sleep(15)
         elif "NOTBEGAN" in status:
+            operateFans(0)
             print("今日は試合がまだのようです。")
             exit()
         elif "OVER" in status:
+            operateFans(0)
             print("試合終了！！！！")
             exit()
         else:
+            operateFans(1)
             print(status + "が打席に立ってます。応援しましょう。")
             playMusic(status)
             sleep(2)
--- ras_operateFans.py
+++ ras_operateFans.py
@@ -1,7 +1,15 @@
-import wiringpi
+#import wiringpi
 
-def operateFans():
-    print("test")
+def initPins():
+    io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_SYS)
+    io.pinMode(18, io.OUTPUT) 
+    return io
+
+def operateFans(io, onoff):
+    if onoff == 1:
+        io.digitalWrite(18, io.HIGH)
+    else:
+        io.digitalWrite(18, io.LOW)
     
 if __name__ == "__main__":
     print("test")
