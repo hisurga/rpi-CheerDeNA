c552d111a460420a30a6e7c3a334621cf493f517
hisurga <hisurga@gmail.com>
2017-06-11 23:49:54 +0900

    use wiringpi2

--- ras_operateFans.py
+++ ras_operateFans.py
@@ -1,21 +1,22 @@
-import wiringpi
+import wiringpi2
 from time import sleep
 
-def initPin():
-    io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_SYS)
-    io.pinMode(18, io.OUTPUT) 
-    return io
+FANPIN = 18
 
-def operateFan(io, onoff):
+def initPin():
+    wiringpi2.wiringPiSetupGpio()
+    wiringpi2.pinMode(FANPIN, 1)
+    
+def operateFan(onoff):
     if onoff == 1:
-        io.digitalWrite(18, io.HIGH)
+        wiringpi2.digitalWrite(FANPIN, 1)
     else:
-        io.digitalWrite(18, io.LOW)
+        wiringpi2.digitalWrite(FANPIN, 0)
     
 if __name__ == "__main__":
-    io = initPin()
+    initPin()
     while True:
-        operateFan(io, 1)
-        sleep(10)
-        operateFan(io, 0)
-        sleep(10)
+        operateFan(1)
+        sleep(1)
+        operateFan(0)
+        sleep(1)
