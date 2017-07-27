cd13d93631f48f90b2278a27c77b6026494927b0
hisurga <hisurga@gmail.com>
2017-07-27 23:23:22 +0900

    refactoring create class

--- ras_fetchGame.py
+++ ras_fetchGame.py
@@ -4,79 +4,88 @@ import requests
 from datetime import datetime
 from time import sleep
 
-DENA = "ＤｅＮＡ"
-OUT3 = "●●●"
-
-def fetchScoreUrl():   
-    today = datetime.now()
-    topUrl = "https://baseball.yahoo.co.jp/npb/schedule/?date={today:%Y%m%d}"
-
-    #取得できないとエラー発生
-    topHtml = requests.get(topUrl).text
-    topRoot = lxml.html.fromstring(topHtml)
-
-    tdTeam = topRoot.xpath('//td[@class="today pl7"]/*[text()="%s"]' %DENA)
-
-    if tdTeam == []:
-        return("NOGAME")
-
-    # trの位置からscoreUrlの検索
-    tr = tdTeam[0].getparent().getparent()
-
-    # scoreUrlの場所はtoday ctの2番目
-    forScoreUrl = tr.xpath('td[@class="today ct"][2]//a/@href')
-
-    if forScoreUrl == []:
-        return("NOTBEGAN")
-    else:
-        return(forScoreUrl[0])
-
-def fetchGameStatus(forScoreUrl):
-    scoreUrl = "http://baseball.yahoo.co.jp/live/" + forScoreUrl + "score"
-
-    #取得できないとエラー発生
-    scoreHtml = requests.get(scoreUrl).text
-    scoreRoot = lxml.html.fromstring(scoreHtml)
+class FetchGame:
     
-    liveNavi = scoreRoot.cssselect('#livenavi p')[0].text_content()
+    def __init__(self):
+        DENA = "ＤｅＮＡ"
+        OUT3 = "●●●"
 
-    outCount = ""
-    
-    if(scoreRoot.xpath('//p[@class="o"]/b') != []):
-        outCount = scoreRoot.xpath('//p[@class="o"]/b')[0].text_content()
+        today = datetime.now()
+        top_url = "https://baseball.yahoo.co.jp/npb/schedule/?date={today:%Y%m%d}"
+
+        #取得できないとエラー発生
+        top_html = requests.get(top_url).text
+        top_root = lxml.html.fromstring(top_html)
+
+        td_team = top_root.xpath('//td[@class="today pl7"]/*[text()="%s"]' %DENA)
+
+        if td_team == []:
+            self.game_status = "NOGAME"
+            return
+
+        # trの位置からscore_urlの検索
+        tr = td_team[0].getparent().getparent()
+
+        # score_urlの場所はtoday ctの2番目
+        for_score_url = tr.xpath('td[@class="today ct"][2]//a/@href')
+
+        if for_score_url == []:
+            self.game_status = "NOTBEGAN"
+
+        else:
+            self.game_status = "NOW"
+            self.game_url = for_score_url[0]
+
+    def fetch_game(self):
+        score_url = "http://baseball.yahoo.co.jp/live/" + self.game_url + "score"
+
+        #取得できないとエラー発生
+        score_html = requests.get(score_url).text
+        score_root = lxml.html.fromstring(score_html)
         
-    if (DENA in liveNavi) and not(OUT3 in outCount):
-        return(scoreRoot.cssselect('#batter span')[0].text_content()[1:])
-    elif "試合終了" in liveNavi:
-        return("OVER")
-    elif "試合前" in liveNavi:
-        return("NOTBEGAN")
-    else:
-        return("OPPONENT")
+        live_navi = score_root.cssselect('#liveNavi p')[0].text_content()
+
+        if(score_root.xpath('//p[@class="o"]/b') != []):
+            out_count = score_root.xpath('//p[@class="o"]/b')[0].text_content()
+
+        else:
+            self.game_status = "NOTBEGAN"
+            return None
+                    
+        if (DENA in live_navi) and not(OUT3 in out_count):
+            return(score_root.cssselect('#batter span')[0].text_content()[1:])
+
+        elif "試合終了" in live_navi:
+            self.game_status = "NOGAME"
+            return None
+
+        elif "試合前" in live_navi:
+            self.game_status = "NOTBEGAN"
+            return None
+
+        else:
+            self.game_status = "OPPONENT"
+            return None
+
+    def get_game_status(self):
+        return self.game_status
     
 if __name__ == "__main__":
-    forScoreUrl = fetchScoreUrl()
-    if not('/' in forScoreUrl):
-        if "NOGAME" in forScoreUrl:
-            print("今日は試合がないようです。")
-        else:
-            print("今日は試合がまだのようです。")
-        exit
+    fg = FetchGame()
+
+    status = fg.get_game_status()
+
+    if not("NOW" in status):
+        print(status)
+        exit()
 
     else:
         while True:
-            status = fetchGameStatus(forScoreUrl)
+            batter = fg.fetch_game()
 
-            if "OPPONENT" in status:
-                print("相手の攻撃中です。")
-                
-            elif "NOTBEGAN" in status:
-                print("今日は試合がまだのようです。")
-                exit()
-            elif "OVER" in status:
-                print("試合終了！！！！")
-                exit()
-            else:
-                print(status + "が打席に立ってます。応援しましょう。")
+            if batter is None:
+                print(fg.get_game_status())
             
+            else:
+                print(batter + "が打席に立ってます。応援しましょう。")
             sleep(10)
