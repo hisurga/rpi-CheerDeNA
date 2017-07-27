e35a80928cd71341f080c77c103f56a81b4d02d4
hisurga <hisurga@gmail.com>
2017-07-27 22:05:15 +0900

    refactoring

--- ras_operateFans.py
+++ ras_operateFans.py
@@ -4,23 +4,14 @@ from time import sleep
 
 FANPIN = 18
 
-def initPin():
-    wiringpi2.wiringPiSetupGpio()
-    wiringpi2.pinMode(FANPIN, 1)
-    
-def operateFan(onoff):
-    if onoff == 1:
-        wiringpi2.digitalWrite(FANPIN, 1)
-    else:
-        wiringpi2.digitalWrite(FANPIN, 0)
-    
-if __name__ == "__main__":
-    initPin()
-    
-    while True:
-        print("ON")
-        operateFan(1)
-        sleep(5)
-        print("OFF")
-        operateFan(0)
-        sleep(5)
+wiringpi2.wiringPiSetupGpio()
+wiringpi2.pinMode(FANPIN, 1)
+
+while True:
+    print("ON")
+    wiringpi2.digitalWrite(FANPIN, 1)
+    sleep(5)
+
+    print("OFF")
+    wiringpi2.digitalWrite(FANPIN, 0)
+    sleep(5)
