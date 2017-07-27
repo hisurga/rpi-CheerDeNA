3aa98f560024069c3d9316bf4d6dea349fbf965d
hisurga <hisurga@gmail.com>
2017-07-27 22:05:46 +0900

    remove ras_operateFans.py

--- ras_cheerDeNA.py
+++ ras_cheerDeNA.py
@@ -2,9 +2,8 @@
 from ras_fetchGame import fetchScoreUrl
 from ras_fetchGame import fetchGameStatus
 from ras_playMusic import playMusic
-from ras_operateFans import initPin
-from ras_operateFans import operateFan
 from time import sleep
+import wiringpi2
 
 forScoreUrl = fetchScoreUrl()
 
@@ -17,25 +16,27 @@ if not('/' in forScoreUrl):
 
 else:
     playMusic("D")
-    initPin()
+
+    wiringpi2.wiringPiSetupGpio()
+    wiringpi2.pinMode(FANPIN, 1)
 
     while True:
         status = fetchGameStatus(forScoreUrl)
         
         if "OPPONENT" in status:
-            operateFan(0)
+            wiringpi2.digitalWrite(FANPIN, 0)
             print("相手の攻撃中です。")
             sleep(15)
         elif "NOTBEGAN" in status:
-            operateFan(0)
+            wiringpi2.digitalWrite(FANPIN, 0)
             print("今日は試合がまだのようです。")
             exit()
         elif "OVER" in status:
-            operateFan(0)
+            wiringpi2.digitalWrite(FANPIN, 0)
             print("試合終了！！！！")
             exit()
         else:
-            operateFan(1)
+            wiringpi2.digitalWrite(FANPIN, 1)
             print(status + "が打席に立ってます。応援しましょう。")
             playMusic(status)
             sleep(2)
